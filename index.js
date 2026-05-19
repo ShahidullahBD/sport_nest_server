dacfa42e6b0224c385c0bb2ecc850a69f23c1386
ShahidullahBD <shahidullahkorea@gmail.com>
2026-05-19 17:55:56 +0900

    All Facilities route done

--- index.js
+++ index.js
@@ -6,15 +6,63 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 
-app.get('/', (req, res)=>{
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const db = client.db('sport_nest_server');
+        const allFacilitiesCollections = db.collection('all_facilities')
+
+        app.get('/all-facilities', async (req, res)=>{
+            const result = await allFacilitiesCollections.find().toArray()
+            res.json(result)
+        })
+
+        app.get('/all-facilities/:id', async (req, res)=>{
+            const {id} = req.params;
+            const result = await allFacilitiesCollections.findOne({_id:new ObjectId(id)})
+            res.json(result)
+        })
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('Server is running Fine!')
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`);
 })
